a24581387ecb39e69d980e3c1e66f1bed5c60b68
Micha Motyka <szortownikq@gmail.com>
2024-02-04 16:15:28 +0100

    fix product names mock

--- suma/src/main/java/com/example/suma/mediator/ProductMediator.java
+++ suma/src/main/java/com/example/suma/mediator/ProductMediator.java
@@ -31,7 +31,6 @@ public class ProductMediator {
         return products.stream().map(productDtoToProduct::toProductDTO).collect(Collectors.toList());
     }
 
-
     public List<ProductDTO> getProducts(HttpServletResponse response, String search, int page, int limit, Sort sort, Order order, String category, Double priceMin, Double priceMax) {
         List<Product> products = productService.getProducts(search, page, limit, sort, order, category, priceMin, priceMax);
         response.addHeader("X-Total-Count", String.valueOf(productService.totalCountProduct(search, page, limit, sort, order, category, priceMin, priceMax)));
