b6e7fa73517c2cd57cd9b726f9de81017fea7114
Micha Motyka <szortownikq@gmail.com>
2024-03-06 19:27:40 +0100

    fix

--- suma/src/main/java/com/example/suma/security/WebSecurity.java
+++ suma/src/main/java/com/example/suma/security/WebSecurity.java
@@ -57,7 +57,7 @@ public class WebSecurity {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200","http://89.168.65.36","http://localhost","http://localhost:3000","https://merch-prod.snd.payu.com","http://89.168.65.36:4200","http://89.168.65.36:3000","185.68.14.10", "185.68.14.11", "185.68.14.12", "185.68.14.26", "185.68.14.27", "185.68.14.28"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","http://89.168.65.36:80","http://localhost","http://localhost:3000","https://merch-prod.snd.payu.com","http://89.168.65.36:4200","http://89.168.65.36:3000","185.68.14.10", "185.68.14.11", "185.68.14.12", "185.68.14.26", "185.68.14.27", "185.68.14.28"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("X-Total-Count","X-Total-Basket-Product-Count","order","X-Location"));
