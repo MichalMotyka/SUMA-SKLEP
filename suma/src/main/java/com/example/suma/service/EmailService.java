d2ed517d73dcdd3f38246485c68c7055a472055e
Micha Motyka <szortownikq@gmail.com>
2024-03-19 13:56:49 +0100

    email service

---
+++ suma/src/main/java/com/example/suma/service/EmailService.java
@@ -0,0 +1,41 @@
+package com.example.suma.service;
+
+import com.example.suma.configuration.EmailConfiguration;
+import com.example.suma.entity.ZMDocument;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+
+    private final EmailConfiguration emailConfiguration;
+
+    @Value("${front.url}")
+    private String fontendUrl;
+
+    @Value("classpath:static/mail-aktywuj.html")
+    private Resource orderTemplate;
+
+    public void sendOrder(ZMDocument zmDocument){
+        log.info("--START sendActivation");
+        try{
+            String html = Files.toString(orderTemplate.getFile(), Charsets.UTF_8);
+            html = html.replace("https://google.com",fontendUrl+"/podsumowanie/"+zmDocument.getUuid());
+            emailConfiguration.sendMail(zmDocument.getEmail(), html,"Złożono zamówienie",true);
+        }catch (IOException e){
+            log.info("Cant send mail");
+            throw new RuntimeException(e);
+        }
+        log.info("--STOP sendActivation");
+    }
+}
